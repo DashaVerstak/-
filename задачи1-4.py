ac80b03f1cdf804cff13ca58b5a401bcbb04c815
DashaVerstak <dashaver2020@gmail.com>
2024-10-16 18:46:42 +0300

    Add files via upload

---
+++ задачи1-4.py
@@ -0,0 +1,30 @@
+'''n=int(input())
+if n>14:
+print ('Добро пожаловать!')
+elif n<14:
+print ('Вам ещё рано!')
+elif n==14:
+print ('Baм 14 лет! Поздравляю!')'''
+
+'''n=input()
+m='secret'
+if n==m:
+print ('Доступ разрешен!')
+else:
+print ('Неверный пароль!')'''
+
+'''n=input()
+if n.isdigit():
+print('Это цифра!')
+elif n.isalpha() :
+print('Это буква!')
+else:
+print ('Это не цифра и не буква!')'''
+
+'''s=float(input())
+d=0
+if s>1000:
+d=0.1
+elif s>500:
+d=0.05
+print(s-(s*d))'''
