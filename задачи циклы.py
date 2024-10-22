41c4d5088630b64c33f2c1ceac99d7ec759f04fb
DashaVerstak <dashaver2020@gmail.com>
2024-10-22 14:53:18 +0300

    Add files via upload

---
+++ задачи циклы.py
@@ -0,0 +1,31 @@
+'''sum=0
+for i in range(5):
+    a=int(input())
+    sum+=a
+print(sum)'''
+
+'''n=int(input())
+a=1
+r=1
+while a<=n:
+    r*=a
+    a+=1
+print(r)'''
+
+
+'''def is_prime(a):
+    if a%a==0 and a!=0:
+        return True
+    else:
+        return False
+a=int(input())
+print(is_prime(a))'''
+
+'''text=input()
+g='АаОоУуЭэЫыЕеЁёЮюЯяИи'
+k=0
+for i in text.lower():
+    if i in g:
+        k+=1
+print(k)'''
+
